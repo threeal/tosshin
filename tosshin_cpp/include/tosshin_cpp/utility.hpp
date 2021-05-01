a8f3d393f1fe574ca693181e4f8e6cc16aaedd35
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-05-01 21:33:42 +0700

    feat: remove position to keisan::point conversion

--- tosshin_cpp/include/tosshin_cpp/utility.hpp
+++ tosshin_cpp/include/tosshin_cpp/utility.hpp
@@ -21,7 +21,6 @@
 #ifndef TOSSHIN_CPP__UTILITY_HPP_
 #define TOSSHIN_CPP__UTILITY_HPP_
 
-#include <keisan/keisan.hpp>
 #include <tosshin_interfaces/msg/maneuver.hpp>
 #include <tosshin_interfaces/msg/odometry.hpp>
 #include <tosshin_interfaces/msg/orientation.hpp>
@@ -38,21 +37,6 @@ using Orientation = tosshin_interfaces::msg::Orientation;
 using Position = tosshin_interfaces::msg::Position;
 using ConfigureOdometry = tosshin_interfaces::srv::ConfigureOdometry;
 using ConfigureManeuver = tosshin_interfaces::srv::ConfigureManeuver;
-
-inline keisan::Point2 position_to_point(const Position & position)
-{
-  return keisan::Point2(position.x, position.y);
-}
-
-inline Position point_to_position(const keisan::Point2 & point)
-{
-  Position position;
-
-  position.x = point.x;
-  position.y = point.y;
-
-  return position;
-}
 
 }  // namespace tosshin_cpp
 
