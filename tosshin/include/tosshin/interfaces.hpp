cc5aa98d719c1fa191e09c068eb18c423fb48e5f
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-06-26 13:20:20 +0700

    feat: add interfaces from external packages

---
+++ tosshin/include/tosshin/interfaces.hpp
@@ -0,0 +1,52 @@
+// Copyright (c) 2021 Alfi Maulana
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef TOSSHIN__INTERFACES_HPP_
+#define TOSSHIN__INTERFACES_HPP_
+
+#include <geometry_msgs/msg/point.hpp>
+#include <geometry_msgs/msg/pose.hpp>
+#include <geometry_msgs/msg/pose_with_covariance.hpp>
+#include <geometry_msgs/msg/quaternion.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <geometry_msgs/msg/twist_with_covariance.hpp>
+#include <geometry_msgs/msg/vector3.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+
+namespace tosshin
+{
+
+namespace msg
+{
+
+using geometry_msgs::msg::Point;
+using geometry_msgs::msg::Pose;
+using geometry_msgs::msg::PoseWithCovariance;
+using geometry_msgs::msg::Quaternion;
+using geometry_msgs::msg::Twist;
+using geometry_msgs::msg::TwistWithCovariance;
+using geometry_msgs::msg::Vector3;
+using nav_msgs::msg::Odometry;
+
+}  // namespace msg
+
+}  // namespace tosshin
+
+#endif  // TOSSHIN__INTERFACES_HPP_
