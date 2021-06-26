e67bd358dbde5ffb44530360587f37e5ec35d9d8
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-06-26 15:00:56 +0700

    feat: add odometry topic argument in the odometry echo

--- tosshin/bin/odometry_echo.cpp
+++ tosshin/bin/odometry_echo.cpp
@@ -18,6 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#include <argparse/argparse.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <tosshin/tosshin.hpp>
 
@@ -30,10 +31,29 @@ int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
 
+  auto program = argparse::ArgumentParser("odometry_echo", "0.3.0");
+
+  program.add_argument("--odometry-topic")
+  .help("name of the odometry topic")
+  .default_value(std::string("/odom"));
+
+  std::string odometry_topic;
+
+  // Try to parse arguments
+  try {
+    program.parse_args(argc, argv);
+
+    odometry_topic = program.get<std::string>("--odometry-topic");
+  } catch (const std::exception & e) {
+    std::cout << e.what() << std::endl;
+    std::cout << program;
+    return 1;
+  }
+
   auto node = std::make_shared<rclcpp::Node>("odometry_echo");
 
   auto odometry_subscription = node->create_subscription<tsn::msg::Odometry>(
-    "/odom", 10,
+    odometry_topic, 10,
     [node](const tsn::msg::Odometry::SharedPtr msg) {
       auto & position = msg->pose.pose.position;
       auto & linear = msg->twist.twist.linear;
